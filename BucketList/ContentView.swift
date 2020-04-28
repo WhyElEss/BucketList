c3028488dcf57a18974e28045bc1de9f1edca9b2
Yuri Stanislavsky <stanislavskiy@gmail.com>
2020-04-28 14:32:13 +0300

    Initial Commit

---
+++ BucketList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Юрий Станиславский on 28.04.2020.
+//  Copyright © 2020 Yuri Stanislavsky. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
